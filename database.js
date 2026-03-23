deac4c3a12b15a397d2046eb5d75f51741087ae0
gianluigi97 <gianluigi.mosti97@gmail.com>
2026-03-23 21:38:08 +0100

    connession db con javascript

---
+++ database.js
@@ -0,0 +1,62 @@
+import pkg from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: 'Nest.env' })
+
+const { Pool } = pkg
+
+class Database {
+
+    constructor(){
+        
+        this.user = process.env.user
+        this.password=process.env.password
+        this.host=process.env.host
+        this.port=process.env.port 
+        this.dbname=process.env.dbname
+        this.pool=null
+    }
+
+    connection() {
+
+        if (this.pool) return this.pool
+
+        
+        this.pool = new Pool ({
+            user: this.user,
+            password: this.password,
+            host: this.host,
+            port: this.port,
+            database: this.dbname,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        })
+
+        return this.pool
+    }
+
+    async getUser() {
+        const pool = this.connection()
+
+        try {
+            const res = await pool.query("SELECT * FROM utenti")
+            return res.rows
+        } catch (err) {
+            console.error("Query error: ", err)
+            throw err
+        }
+    }
+}
+
+export default Database
+
+
+const db = new Database()
+const users = await db.getUser()
+const nomi = users.map(row => row.nome)
+
+
+
+
+console.log(nomi)
